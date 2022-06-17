a9f46c78fcfeb8c61fc5b7c43cbda3fcaf162b14
ArmenAyrapetyan <manho2002@mail.ru>
2022-06-17 13:59:56 +0500

    feature_interface|Unload product info complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,15 @@
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-                @for($i=0; $i<50; $i++)
+                @foreach($products as $product)
                 <div class="col">
                     <div class="card shadow-sm border-top rounded-top">
-                        <svg class="bd-placeholder-img card-img-top rounded-top" width="100%" height="225"
-                             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                             preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c"/>
-                            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
+                        <img class="card-img" src="{{asset(('storage/images/' . $product->image))}}" alt="product"/>
 
                         <div class="card-body border-end border-bottom border-start border-primary rounded-bottom">
-                            <p class="card-text">Категория</p>
-                            <p class="card-text">7250 <small class="text-muted">/ ₽</small></p>
-                            <p class="card-text">Описание</p>
+                            <p class="card-text">{{ $product->type->name }}</p>
+                            <p class="card-text">{{ $product->price }} <small class="text-muted">/ ₽</small></p>
+                            <p class="card-text">{{ $product->description }}</p>
 
                             <div class="d-flex justify-content-end align-items-center">
                                 <div class="btn-group">
@@ -31,7 +26,7 @@
                         </div>
                     </div>
                 </div>
-                @endfor
+                @endforeach
 
             </div>
         </div>
